2f2764195660fb4c65247686ec1a0683ee0529be
Thiago Nascimento <thiagodnf@gmail.com>
2012-04-19 01:54:27 -0300

    Algumas correções de bug

--- src/MiniJava.java
+++ src/MiniJava.java
@@ -1,35 +1,56 @@
 import java.io.File;
-
 import org.minijava.gui.MiniJavaGUI;
 import org.minijava.tests.ScannerTest;
+import org.apache.commons.cli.*;
 
 public class MiniJava {
+
+	private static void printHeader() {
+		System.out.println("===========================================");
+		System.out.println("MiniJava Compiler");
+		System.out.println("Version:  1.0");
+		System.out.println("Authors: Débora Martins e Thiago Nascimento");
+		System.out.println("===========================================");
+		System.out.println("Welcome");
+	}
 	
-	private static void header() {
-		System.out.println("===========================================");
-		System.out.println("Compilador para a linguagem MiniJava");
-		System.out.println("Versão:  1.0");
-		System.out.println("Autores: Débora Martins e Thiago Nascimento");
-		System.out.println("===========================================");
+	private static void printVersion(){
+		System.out.println("MiniJava Compiler Version: 1.0");
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		header();
-		
-		if(args[0].equals("-gui")){
-			MiniJavaGUI gui = new MiniJavaGUI();
-			gui.run();
-		}else if(args[0].equals("-d")){
-			ScannerTest scanner = new ScannerTest(new File(args[1]));
-			scanner.run();
-		}else{
-			System.out.println("Example: -gui or -d path/code.java");
-			return;
-		}
+		Options options = new Options();
+		File code = null;
+		options.addOption("gui", false, "Load Graphics Interface");
+		options.addOption("d", true, "Code's path");
+		options.addOption("v", false, "MiniJava Compiler Version");
+
+		CommandLineParser parser = new PosixParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);	
+			
+			printHeader();
+			
+			if (cmd.hasOption("d")){
+				code = new File(cmd.getOptionValue("d"));
+			}
+			if(cmd.hasOption("v")){
+				printVersion();
+			}
+			if (cmd.hasOption("gui")){
+				MiniJavaGUI gui = new MiniJavaGUI(code);
+				gui.run();
+			}else{
+				ScannerTest scanner = new ScannerTest(code);
+				scanner.run();
+			}	
+		} catch (ParseException e) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("MiniJava Compiler",options);
+		}				
 	}
 
-	
 }
