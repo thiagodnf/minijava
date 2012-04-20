c71b487873b8e436ee96f346fab9874516d6252f
Home <debora@ubuntu.(none)>
2012-04-19 22:04:22 -0300

    Adicionado arquivos teste com erros.

---
+++ examples/fail/Example.java
@@ -0,0 +1,10 @@
+//A Very Simple Example
+class ExampleProgram {
+   for(int i =0;;i++){
+	a=j--;
+	System.out.println(Erro total);
+}
+  public static void main(String[] args){
+    System.out.println(I'm a Simple Program);
+  }
+}
